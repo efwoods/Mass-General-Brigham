d7c3568b87d005658e97bbe78234870c7c1b7d1f
efwoods <e.woods.business@icloud.com>
2022-06-22 17:14:34 -0400

    Python 1.2 start

--- Python/1.2/main.py
+++ Python/1.2/main.py
@@ -1,11 +1,19 @@
+import string, sys, argparse, bs4, requests
 import bs4, requests
 
+url = 'https://www.twitch.tv/vixxxie'
+tags = []
+
+def extract(url, tags):
+    res = requests.get(url);
+    res.raise_for_status();
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+#    elems = soup.select(tags)
+#    print(elems[0].text.strip())
 
 if __name__ == '__main__':
-    paparser = argparse.ArgumentParser(description='Extract')
-    parser.add_argument('first_number', type=float)
-    parser.add_argument('second_number', type=float)
-    parser.add_argument('third_number', type=float)
-    args = parser.parse_args()
-    result = maximumAbsoluteOfThree(args.first_number, args.second_number, args.third_number)
-    print(result)
+#    parser = argparse.ArgumentParser(description='Extract')
+#    parser.add_argument('url', type=string)
+#    args = parser.parse_args()
+    extract(url,tags);
+
