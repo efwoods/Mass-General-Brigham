8cb9db0a563ed059da8d17e71baa4e56d71ad33c
efwoods <e.woods.business@icloud.com>
2022-06-22 13:30:34 -0400

    stop python 1.2; for break

--- Python/1.2/main.py
+++ Python/1.2/main.py
@@ -1,1 +1,11 @@
+import bs4, requests
 
+
+if __name__ == '__main__':
+    paparser = argparse.ArgumentParser(description='Extract')
+    parser.add_argument('first_number', type=float)
+    parser.add_argument('second_number', type=float)
+    parser.add_argument('third_number', type=float)
+    args = parser.parse_args()
+    result = maximumAbsoluteOfThree(args.first_number, args.second_number, args.third_number)
+    print(result)
